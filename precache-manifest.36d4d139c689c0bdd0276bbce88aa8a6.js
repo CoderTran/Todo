720637b8378513533ea645a8e20ca0ea18efa403
CoderTran <tranquochieugl2@gmail.com>
2023-04-04 18:03:34 +0900

    Updates

---
+++ precache-manifest.36d4d139c689c0bdd0276bbce88aa8a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6106f8526a642add65b206fbf8c1fdd0",
+    "url": "/Todoapp/index.html"
+  },
+  {
+    "revision": "b064714a8b70d565f027",
+    "url": "/Todoapp/static/css/main.e16238b7.chunk.css"
+  },
+  {
+    "revision": "5dc6657d076cebf0d1b1",
+    "url": "/Todoapp/static/js/2.a80ec655.chunk.js"
+  },
+  {
+    "revision": "f390729f3ec498aec1a683ee43ec2ccd",
+    "url": "/Todoapp/static/js/2.a80ec655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b064714a8b70d565f027",
+    "url": "/Todoapp/static/js/main.4803d4c8.chunk.js"
+  },
+  {
+    "revision": "14a5e52b6d236df3dd69",
+    "url": "/Todoapp/static/js/runtime-main.02a341f5.js"
+  }
+]);
