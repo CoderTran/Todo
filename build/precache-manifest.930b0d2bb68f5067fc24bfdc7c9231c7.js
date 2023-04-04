aa0f3cbae1e4fca055f9c4137b12fb7b8ecfe9b4
CoderTran <tranquochieugl2@gmail.com>
2023-04-04 14:49:35 +0900

    das

---
+++ build/precache-manifest.930b0d2bb68f5067fc24bfdc7c9231c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e181797d5bc82f8a26e0f98f6c564d81",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "cd5a1125604d1828ccf8",
+    "url": "/Todo/static/css/main.d2ec8e4d.chunk.css"
+  },
+  {
+    "revision": "0b3a321ca383d987e664",
+    "url": "/Todo/static/js/2.7ad6d5ce.chunk.js"
+  },
+  {
+    "revision": "f390729f3ec498aec1a683ee43ec2ccd",
+    "url": "/Todo/static/js/2.7ad6d5ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd5a1125604d1828ccf8",
+    "url": "/Todo/static/js/main.4803d4c8.chunk.js"
+  },
+  {
+    "revision": "b75d342943a3e704d238",
+    "url": "/Todo/static/js/runtime-main.812114fc.js"
+  }
+]);
